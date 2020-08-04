67dbc6b597e6b2a6e30ae8f8bae2fe01e66c8d49
cormacosullivan <cormac.osullivan123@gmail.com>
2020-08-04 16:30:32 -0700

    Fixed problem with bot not replying to tweets

--- Main.py
+++ Main.py
@@ -3,69 +3,83 @@
 
 import tweepy
 import logging
-from config import create_api
+#from config import create_api
 import time
 import json
 import requests
+import os
+
 
 
 
 BOT_HANDLE = ""
 JSON_PATH = "venv/Include/city.list.json"
 OPENWEATHERMAP_API_KEY = "f7fc9317623016c29feedd9488e0bbee"
-KEYWORDS = ["LOCATION:"]
+KEYWORDS = ["location:"]
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 def send_api_request(location):
-    call = f"api.openweathermap.org/data/2.5/weather?q={location}&appid={OPENWEATHERMAP_API_KEY}"
-    response = requests.get(url)
+    print(location)
+    call = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={OPENWEATHERMAP_API_KEY}"
+    response = requests.get(call)
     data = json.loads(response.text)
     return data
 
 def parse_api_response(data):
+    print(data)
     location = data["name"]
-    description = data["weather"]["description"]
-    temp = data["main"]["temp"]
-    feels_like = data["main"]["feels_like"]
-    temp_min = data["main"]["temp_min"]
-    temp_max = data["main"]["temp_max"]
-    humidity = data["main"]["humidity"]
-    sunrise = data["main"]["sunrise"]
-    sunset = data["main"]["sunset"]
-    info_string = f"Here is the weather results for {name} are as follows:\nDescription: {description}\nTemperature: {temp}\nFeels like: {feels_like}\nMin: {temp_min}\nMax: {temp_max}\nHumidity: {humidity}\nSunrise: {sunrise}\nSunset: {sunset}"
+    description = data[0]["weather"]["description"]
+    temp = data[0]["main"]["temp"]
+    feels_like = data[0]["main"]["feels_like"]
+    temp_min = data[0]["main"]["temp_min"]
+    temp_max = data[0]["main"]["temp_max"]
+    humidity = data[0]["main"]["humidity"]
+    sunrise = data[0]["main"]["sunrise"]
+    sunset = data[0]["main"]["sunset"]
+    info_string = f"Here is the weather results for {location} are as follows:\nDescription: {description}\nTemperature: {temp}\nFeels like: {feels_like}\nMin: {temp_min}\nMax: {temp_max}\nHumidity: {humidity}\nSunrise: {sunrise}\nSunset: {sunset}"
+    print(info_string)
     return info_string
 
 def check_mentions(api, keywords, since_id,city_data):
     logger.info("Retrieving mentions")
     new_since_id = since_id
-    for tweet in tweepy.Cursor(api.mentions_timeline,
-        since_id=since_id).items():
+
+    for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
+        print("Mentioned")
+        print("new since id:",new_since_id)
+        print("tweet.id",tweet.id)
         new_since_id = max(tweet.id, new_since_id)
+        print("new since id:",new_since_id)
+
         if tweet.in_reply_to_status_id is not None:
             continue
         if any(keyword in tweet.text.lower() for keyword in keywords):
             logger.info(f"Answering to {tweet.user.name}")
             status = tweet.text.lower().split()
+            print("\n\nStatus: ",status)
             try:
-                location_index = status.index("LOCATION:") + 1
-                if status[location_index] not in city_data:
-                    raise Exception
+                location_index = status.index(keywords[0]) + 1
+                print("\n\nLocation index",location_index)
+                # if status[location_index] not in city_data:
+                #     raise Exception
                 api_answer = send_api_request(status[location_index])
                 tweet_string = parse_api_response(api_answer)
             except Exception as e:
+                print(e)
                 logger.error("Invalid tweet recived")
-                invalid_mention_reply = f"Unfortunatly there is no available forcast for: {status[location_index]}\nThe current forcast for the Moon is: Clear skies with a 0% chance of precipitation"
-                api.update_status(
-                    status=invalid_mention_reply,
-                    in_reply_to_status_id=tweet.id,
-                )
-            else:
-                api.update_status(
-                    status=tweet_string,
-                    in_reply_to_status_id=tweet.id,
-                )
+                tweet_string = f"Unfortunatly there is no available forcast for: {status[location_index]}\nThe current forcast for the Moon is: Clear skies with a 0% chance of precipitation"
+            #tweet_string += f" @{tweet.user}"
+            print(len(tweet_string), tweet_string)
+            #tweet_string = f" @{tweet.user} TEST"
+
+            api.update_status(
+                status=tweet_string,
+                in_reply_to_status_id=tweet.id,
+                auto_populate_reply_metadata=True
+            )
+    print("checked")
     return new_since_id
 
 def create_api():
@@ -78,6 +92,7 @@ def create_api():
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True,
         wait_on_rate_limit_notify=True)
+    print(consumer_key,consumer_secret,access_token,access_token_secret)
     try:
         api.verify_credentials()
     except Exception as e:
@@ -101,7 +116,7 @@ def main():
     city_data = read_json(JSON_PATH)
     since_id = 1
     while True:
-        since_id = check_mentions(api, KEYWORDS, since_id,city_data)
+        since_id = check_mentions(api, KEYWORDS, since_id, city_data)
         logger.info("Waiting...")
         time.sleep(60)
 
--- Test.py
+++ Test.py
@@ -13,9 +13,15 @@
 #     print("Authentication OK")
 # except:
 #     print("Error during authentication")
+import os
+# os.environ['CONSUMER_KEY'] = 'EKOaSg962MqCgSq2vCWhiJWMC'
+# os.environ['CONSUMER_SECRET'] = 'cvBEvjJFRIcnkEvVW1pqGL0PLRrTEVWO704k8o68ZuhJ1do1lY'
+# os.environ['ACCESS_TOKEN'] = '1288894648547598337-Fw6ldmhks4fswO9G427Su9FG5akw2p'
+# os.environ['ACCESS_TOKEN_SECRET'] = 'aC40yi31XX35HFFeOhmTLlhbTdvO89sLprdiFWCCJejee'
 
-try:
-    print("this is a test")
-    raise Exception
-except Exception as e:
-    print("excpeted")
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+print(consumer_key, consumer_secret, access_token, access_token_secret)
