784bd243cbba5e9d65ed5636653d867b775d3819
cormacosullivan <cormac.osullivan123@gmail.com>
2020-08-05 10:49:51 -0700

    Fixed problem with openweather api call not being sent Full pipeline now complete

--- Main.py
+++ Main.py
@@ -3,16 +3,13 @@
 
 import tweepy
 import logging
-#from config import create_api
+# from config import create_api
 import time
 import json
 import requests
 import os
+from datetime import datetime
 
-
-
-
-BOT_HANDLE = ""
 JSON_PATH = "venv/Include/city.list.json"
 OPENWEATHERMAP_API_KEY = "f7fc9317623016c29feedd9488e0bbee"
 KEYWORDS = ["location:"]
@@ -20,67 +17,65 @@ KEYWORDS = ["location:"]
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-def send_api_request(location):
-    print(location)
-    call = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={OPENWEATHERMAP_API_KEY}"
+
+def send_api_request(location, units="metric"):
+    call = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={OPENWEATHERMAP_API_KEY}&units={units}"
     response = requests.get(call)
     data = json.loads(response.text)
     return data
 
-def parse_api_response(data):
-    print(data)
-    location = data["name"]
-    description = data[0]["weather"]["description"]
-    temp = data[0]["main"]["temp"]
-    feels_like = data[0]["main"]["feels_like"]
-    temp_min = data[0]["main"]["temp_min"]
-    temp_max = data[0]["main"]["temp_max"]
-    humidity = data[0]["main"]["humidity"]
-    sunrise = data[0]["main"]["sunrise"]
-    sunset = data[0]["main"]["sunset"]
-    info_string = f"Here is the weather results for {location} are as follows:\nDescription: {description}\nTemperature: {temp}\nFeels like: {feels_like}\nMin: {temp_min}\nMax: {temp_max}\nHumidity: {humidity}\nSunrise: {sunrise}\nSunset: {sunset}"
-    print(info_string)
-    return info_string
 
-def check_mentions(api, keywords, since_id,city_data):
+def parse_api_response(data, units="°C"):
+    try:
+        location = data["name"]
+        description = data["weather"][0]["description"].title()
+        temp = data["main"]["temp"]
+        feels_like = data["main"]["feels_like"]
+        temp_min = data["main"]["temp_min"]
+        temp_max = data["main"]["temp_max"]
+        humidity = data["main"]["humidity"]
+        timezone = data["timezone"]
+        # wind_speed = data["wind"]["speed"]
+        # wind_deg = data["wind"]["deg"]
+        sunrise = datetime.utcfromtimestamp(data["sys"]["sunrise"] + timezone).strftime('%H:%M')
+        sunset = datetime.utcfromtimestamp(data["sys"]["sunset"] + timezone).strftime('%H:%M')
+
+        infostring = f"Here is the weather results for {location}:\n\nDescription: {description}\nTemperature: {temp}{units}\nFeels like: {feels_like}{units}\nMin: {temp_min}{units}\nMax: {temp_max}{units}\nHumidity: {humidity}%\nSunrise: {sunrise}\nSunset: {sunset}"
+    except Exception as e:
+        print(e)
+        return ""
+    return infostring
+
+
+def check_mentions(api, keywords, since_id, city_data):
     logger.info("Retrieving mentions")
     new_since_id = since_id
 
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
-        print("Mentioned")
-        print("new since id:",new_since_id)
-        print("tweet.id",tweet.id)
         new_since_id = max(tweet.id, new_since_id)
-        print("new since id:",new_since_id)
-
         if tweet.in_reply_to_status_id is not None:
             continue
         if any(keyword in tweet.text.lower() for keyword in keywords):
             logger.info(f"Answering to {tweet.user.name}")
             status = tweet.text.lower().split()
-            print("\n\nStatus: ",status)
             try:
                 location_index = status.index(keywords[0]) + 1
-                print("\n\nLocation index",location_index)
                 # if status[location_index] not in city_data:
                 #     raise Exception
                 api_answer = send_api_request(status[location_index])
                 tweet_string = parse_api_response(api_answer)
             except Exception as e:
                 print(e)
-                logger.error("Invalid tweet recived")
-                tweet_string = f"Unfortunatly there is no available forcast for: {status[location_index]}\nThe current forcast for the Moon is: Clear skies with a 0% chance of precipitation"
-            #tweet_string += f" @{tweet.user}"
-            print(len(tweet_string), tweet_string)
-            #tweet_string = f" @{tweet.user} TEST"
+                logger.error("Invalid tweet received")
+                tweet_string = f"Unfortunately there is no available forecast for: {status[location_index]}\nThe current forcast for the Moon is: Clear skies with a 0% chance of precipitation"
 
             api.update_status(
                 status=tweet_string,
                 in_reply_to_status_id=tweet.id,
                 auto_populate_reply_metadata=True
             )
-    print("checked")
     return new_since_id
+
 
 def create_api():
     consumer_key = os.getenv("CONSUMER_KEY")
@@ -91,8 +86,7 @@ def create_api():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True,
-        wait_on_rate_limit_notify=True)
-    print(consumer_key,consumer_secret,access_token,access_token_secret)
+                     wait_on_rate_limit_notify=True)
     try:
         api.verify_credentials()
     except Exception as e:
@@ -101,6 +95,7 @@ def create_api():
 
     logger.info("API created")
     return api
+
 
 def read_json(path):
     try:
@@ -111,6 +106,7 @@ def read_json(path):
         return -1
     return data
 
+
 def main():
     api = create_api()
     city_data = read_json(JSON_PATH)
@@ -119,6 +115,7 @@ def main():
         since_id = check_mentions(api, KEYWORDS, since_id, city_data)
         logger.info("Waiting...")
         time.sleep(60)
+
 
 if __name__ == "__main__":
     main()
